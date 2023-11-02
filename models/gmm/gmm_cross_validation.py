1d91ee37842330a093688405392b8220292d7128
PaolaMts <127331331+PaolaMts@users.noreply.github.com>
2023-11-02 17:49:08 +0100

    change cross_validation

--- models/gmm/gmm_cross_validation.py
+++ models/gmm/gmm_cross_validation.py
@@ -4,7 +4,7 @@ from general.utils import k_fold, compute_min_dcf
 import os
 
 def cross_validation_for_gmm(d, l, model, zscore, pca):
-    gmm_values = [range(6)]
+    gmm_values = [range(1, 7)]
     for gmm in gmm_values:
         model.set_values(2**gmm)
         print(f"g_num = {model.g_num} ")
@@ -30,21 +30,21 @@ def compute_min_dcf_for_all_gmm(l, model, zscore, pca, name):  # name = model + 
         os.makedirs("min_dcf_models/" + model.folder())
     np.save(f"min_dcf_models/{model.folder()}/{name}{f"pca_{pca}" if pca else ""}{"_zscore" if zscore else ""}", min_dcf_list)
 
-def cv_gmm_domain(d, l, zscore=False, pca=None):
-    gmm = GMM(preprocess="z_score" if zscore else ""+"pca" if pca is not None else "")
-    cross_validation_for_gmm(d, l, gmm, zscore, pca)
-
-def cv_gmm_Tied(d, l, zscore=False, pca=None):
-    gmm_tied = GMMTied(preprocess="z_score" if zscore else ""+"pca" if pca is not None else "")
-    cross_validation_for_gmm(d, l, gmm_tied, zscore, pca)
-
-def cv_gmm_Diag(d, l, zscore=False, pca=None):
-    gmm_diag = GMMDiag(preprocess="z_score" if zscore else ""+"pca" if pca is not None else "")
-    cross_validation_for_gmm(d, l, gmm_diag, zscore, pca)
-
-def cv_gmm_TiedDiag(d, l, zscore=False, pca=None):
-    gmm_tied_diag = GMMTiedDiag(preprocess="z_score" if zscore else ""+"pca" if pca is not None else "")
-    cross_validation_for_gmm(d, l, gmm_tied_diag, zscore, pca)
+# def cv_gmm_domain(d, l, zscore=False, pca=None):
+#     gmm = GMM(preprocess="z_score/" if zscore else "raw/"+"pca" if pca is not None else "raw")
+#     cross_validation_for_gmm(d, l, gmm, zscore, pca)
+#
+# def cv_gmm_Tied(d, l, zscore=False, pca=None):
+#     gmm_tied = GMMTied(preprocess="z_score/" if zscore else "raw/"+"pca" if pca is not None else "raw")
+#     cross_validation_for_gmm(d, l, gmm_tied, zscore, pca)
+#
+# def cv_gmm_Diag(d, l, zscore=False, pca=None):
+#     gmm_diag = GMMDiag(preprocess="z_score/" if zscore else "raw/"+"pca" if pca is not None else "raw")
+#     cross_validation_for_gmm(d, l, gmm_diag, zscore, pca)
+#
+# def cv_gmm_TiedDiag(d, l, zscore=False, pca=None):
+#     gmm_tied_diag = GMMTiedDiag(preprocess="z_score/" if zscore else "raw/"+"pca" if pca is not None else "raw")
+#     cross_validation_for_gmm(d, l, gmm_tied_diag, zscore, pca)
 
 def gmm_pca(d, l, model, zscore=False):
     for pca in [None, 10, 11, 12]:
@@ -52,10 +52,10 @@ def gmm_pca(d, l, model, zscore=False):
             print("____without PCA_____")
         else:
             print(f"____PCA: {pca}_____")
-            if zscore:
-                model.set_preprocess("zscore/pca")
-            else:
-                model.set_preprocess("raw/pca")
+            # if zscore:
+            #     model.set_preprocess("zscore/pca")
+            # else:
+        model.set_preprocess("z_score/" if zscore else "raw/"+"pca" if pca is not None else "raw")
         cross_validation_for_gmm(d, l, model, zscore, pca)
         model.__init__()
 
